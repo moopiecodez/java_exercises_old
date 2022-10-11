e051bc41e02d92bda78e4da409f64e9940a9f1dc
bbk-mvande <115579277+bbk-mvande@users.noreply.github.com>
2022-10-11 18:40:28 +0100

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,15 @@
+public class Exercise2
+{
+	public static void main(String[] args)
+	{
+	System.out.println("the program is meant to run a while loop while i is smaller than 6 but initialises i as 10 so will never run");
+	/* 
+		int i = 10;
+		while (i < 5) {
+		i++;
+		System.out.println(i);
+		}
+	*/
+
+	}
+}
